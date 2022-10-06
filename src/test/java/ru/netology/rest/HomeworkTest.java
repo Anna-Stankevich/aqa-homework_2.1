05a9a3c1d963e241827eed3f080b72a51b1b4527
Анна Станкевич <stankevich.anna.94@mail.ru>
2022-10-06 15:10:20 +0300

    add_main_files

---
+++ src/test/java/ru/netology/rest/HomeworkTest.java
@@ -0,0 +1,27 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+class HomeworkTest {
+    @Test
+    void MustAcceptDataForStorage() {
+// Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("this is homework") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("this is homework"))
+        ;
+
+    }
+
+}
